2c0fa4748ecbf6c0ccab949990a6658323261dd5
LuthandoNgombane <luthandongombane@gmail.com>
2025-03-13 22:32:13 +0200

    First take of five for week 1

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        //LN - Assking the user for their first and last name
+        Console.WriteLine("What is your first name?");
+        string firstName = Console.ReadLine();
+
+        Console.WriteLine("What is your last name?");
+        string lastName = Console.ReadLine();
+
+        //LN - Displaying the user's names using string interpolation
+        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}!");
+
     }
 }
